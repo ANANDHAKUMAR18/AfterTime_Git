e1d9439b1d31dbc9fe65dfc1553e226b493f4ac2
Sandhiya0205 <146726602+Sandhiya0205@users.noreply.github.com>
2023-11-14 20:29:13 +0530

    Update Maths.java

--- src/test/java/aftertimegit/Maths.java
+++ src/test/java/aftertimegit/Maths.java
@@ -7,14 +7,18 @@ public class Maths {
 	int result =a+b;
 	System.out.println("Sum of two numbers =" + result);
 	}
+	public void sub(int a,int b){
+	int result =a-b;
+	System.out.println("Sub of two numbers =" + result);
+	}
 	
 	
 	
 	
-	
-	public static void main(String[] args){
+    public static void main(String[] args){
     Maths obj=new Maths();
-	obj.add(3,0);
+    obj.add(3,0);
+    obj.sub(3,0);
 	}
 	
 
