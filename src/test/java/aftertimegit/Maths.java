8e4e98901182a586ce22f4b5a771447c5620ede4
anandhakumarg <anandhsandy.g@gmail.com>
2023-11-14 20:21:02 +0530

    This is first Commit

---
+++ src/test/java/aftertimegit/Maths.java
@@ -0,0 +1,21 @@
+package aftertimegit;
+
+public class Maths {
+	
+
+	public void add(int a,int b){
+	int result =a+b;
+	System.out.println("Sum of two numbers =" + result);
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args){
+    Maths obj=new Maths();
+	obj.add(3,0);
+	}
+	
+
+}
